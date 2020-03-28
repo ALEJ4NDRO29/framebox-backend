ed4f5f72ce63fb372596b160e7a80ec3c821bf0b
Alejandro <josealejandro.r.29@gmail.com>
2020-03-28 18:59:15 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var api = require('./routes/api');
+
+require('dotenv').config()
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', api);
+
+// require('./models/User');
+
+/// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// development error handler
+// will print stacktrace
+app.use(function (err, req, res, next) {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({
+        'errors': {
+            message: err.message,
+            error: err
+        }
+    });
+});
+
+
+module.exports = app;
